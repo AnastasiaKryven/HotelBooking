e25796cd095e8a2b265933b1d6df8f83d72fbda3
AnastasiaKryven <36449168+AnastasiaKryven@users.noreply.github.com>
2019-07-03 19:23:39 +0300

    Add files via upload

---
+++ Hotel.WebUI/Models/ReviewViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Hotel.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hotel.WebUI.Models
+{
+    public class ReviewViewModel
+    {
+        public IEnumerable<Review> Reviews { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+
+    }
+}
---
+++ Hotel.WebUI/Models/RoomViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Domain.Entities;
+using Hotel.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Hotel.WebUI.Models
+{
+    public class RoomViewModel
+    {
+        public IEnumerable<Room> Rooms { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
