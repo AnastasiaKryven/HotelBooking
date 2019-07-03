a74f3fd7df0b135ff3f8e5bc3163c4f9b2fbb4d4
AnastasiaKryven <36449168+AnastasiaKryven@users.noreply.github.com>
2019-07-03 19:28:29 +0300

    Add files via upload

---
+++ Hotel.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hotel.WebUI.Startup))]
+namespace Hotel.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
